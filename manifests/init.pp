0ca08312410f40ccec9ffe498b345e6930cb2848
Rob Reynolds <ferventcoder@gmail.com>
2015-02-18 10:41:31 -0600

    Simple template module
    This is a repro of PUP-927 with templates

---
+++ manifests/init.pp
@@ -0,0 +1,50 @@
+class simple_templates {
+
+  case $::operatingsystem {
+    windows: { $temp_dir = 'c:/temp' }
+    default: { $temp_dir = '/tmp' }
+  }
+
+
+  file {["$temp_dir","$temp_dir/newlines"]:
+    ensure => directory,
+    source_permissions => ignore,
+  }
+
+  file {"$temp_dir/newlines/crlf_newlines_template.txt":
+    ensure => file,
+    content => template("simple_templates/crlf_newlines.erb"),
+    source_permissions => ignore,
+  } 
+
+  file {"$temp_dir/newlines/unix_newlines_template.txt":
+    ensure => file,
+    content => template("simple_templates/unix_newlines.erb"),
+    source_permissions => ignore,
+  } 
+
+  file {"$temp_dir/newlines/mixed_newlines_template.txt":
+    ensure => file,
+    content => template("simple_templates/mixed_newlines.erb"),
+    source_permissions => ignore,
+  } 
+
+  file {"$temp_dir/newlines/crlf_newlines_file.txt":
+    ensure => file,
+    source => "puppet:///modules/simple_templates/crlf_newlines.txt",
+    source_permissions => ignore,
+  }
+
+  file {"$temp_dir/newlines/unix_newlines_file.txt":
+    ensure => file,
+    source => "puppet:///modules/simple_templates/unix_newlines.txt",
+    source_permissions => ignore,
+  }
+
+  file {"$temp_dir/newlines/mixed_newlines_file.txt":
+    ensure => file,
+    source => "puppet:///modules/simple_templates/mixed_newlines.txt",
+    source_permissions => ignore,
+  }
+
+}
